864e670948e60c185e88fb59d981ebe49865d068
Bruno Martínez <bmartinezg@minsait.com>
2020-03-26 18:45:58 +0100

    eliminacion de problema de cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,23 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+// // //
+// CORS
+// // //
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+  app.options('*', (req, res) => {
+    // allowed XHR methods
+    res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+    res.send();
+  });
+});
+// // //
 // Body Parser
 // // //
 app.use(bodyParser.urlencoded({ extended: false }))
